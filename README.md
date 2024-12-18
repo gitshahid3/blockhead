9ca3d6267912fee5a52d4479364d2503745d9329
gitshahid3 <mohammadshahid3790@gmail.com>
2024-12-18 07:29:19 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+put infura api key in environment as INFURA_API_KEY
---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { MetaMaskProvider } from "@metamask/sdk-react";
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <MetaMaskProvider
+      sdkOptions={{
+        dappMetadata: {
+          name: "Blockhead",
+          url: window.location.href,
+        },
+        infuraAPIKey: process.env.INFURA_API_KEY
+      }}
+    >
+      <App />
+    </MetaMaskProvider>
+  </StrictMode>,
+)
